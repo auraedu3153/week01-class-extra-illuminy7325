f9de47d2e8153d6ce9d9f3bacb39d1c05f2add47
kodingk <kodingk3153@gmail.com>
2026-01-02 20:08:05 +0900

    problem 01

---
+++ problem_01.py
@@ -0,0 +1,6 @@
+def snail_board(row: int, col: int, clockwise=True) -> list[list[int]]:
+    """
+    행 개수 (row)와 열 개수 (col)을 받아 달팽이 배열을 만드는 함수.
+    시작 위치는 (0, 0)이고, 방향은 인자 clockwise에 따라 시계방향 또는 반시계방향으로 설정됨.
+    """
+    raise NotImplementedError
